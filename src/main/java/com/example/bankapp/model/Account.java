9a309da4620e8ae202de2483a2b424e014deee21
saad.qayyum <khawajasaadqayyum4110@gmail.com>
2024-08-29 14:41:22 +0300

    Model and repo classes are added successfully

--- src/main/java/com/example/bankapp/model/Account.java
+++ src/main/java/com/example/bankapp/model/Account.java
@@ -1,19 +1,31 @@
 package com.example.bankapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import jakarta.transaction.Transaction;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Data
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
+    private String username;
+    private String password;
+    private BigDecimal balacne;
+    @OneToMany(mappedBy = "account")
+    private List<Transaction> transaction;
+// @Transient is used if we don't want to create a columns in database
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
 }
